e48db43fa8700dcddcedd5a69d42d95de481a475
Erik Behrends <behrends@users.noreply.github.com>
2017-09-25 13:13:48 +0200

    RNBook: files for chapter 5

---
+++ react-native-buch/Kapitel5/JournalItems.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  SectionList,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+const TouchableItem =
+  Platform.OS === 'ios' ? TouchableOpacity : TouchableNativeFeedback;
+
+export default class JournalItems extends Component {
+  render() {
+    if (this.props.items.length === 0)
+      return (
+        <View style={styles.noItems}>
+          <Text style={styles.infoText}>Keine Einträge im Tagebuch</Text>
+        </View>
+      );
+
+    return (
+      <SectionList
+        style={styles.list}
+        sections={this.props.items}
+        renderItem={({ item }) => (
+          <TouchableItem>
+            <View>
+              <Text>{item.text}</Text>
+            </View>
+          </TouchableItem>
+        )}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.listHeader}>{section.title}</Text>
+        )}
+        keyExtractor={item => item.date}
+        ItemSeparatorComponent={() => (
+          <View style={styles.listSeparator} />
+        )}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  infoText: {
+    color: 'darkslategray',
+    fontSize: 22,
+    fontWeight: '300'
+  },
+  noItems: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  list: {
+    marginTop: 24
+  },
+  listHeader: {
+    color: 'gray',
+    fontWeight: '600',
+    backgroundColor: 'lightcyan',
+    paddingHorizontal: 5
+  },
+  listSeparator: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: 'lightblue'
+  }
+});

dffd6e2eca492f9f1fc0247449c77622f9844908
Erik Behrends <behrends@users.noreply.github.com>
2017-09-26 15:13:24 +0200

    RNBook: some files for chapter 6

---
+++ react-native-buch/Kapitel6/JournalItemInput/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import JournalItems from './js/components/JournalItems';
+import JournalItemInput from './js/components/JournalItemInput';
+
+const journalItems = [];
+
+export default class App extends Component {
+  state = { items: journalItems };
+
+  _addItem(text) {
+    let { items } = this.state;
+    let [head, ...tail] = items;
+
+    const now = new Date();
+    const day = now.getDate();
+    const month = now.getMonth() + 1;
+    const year = now.getFullYear();
+    const today = `${day}.${month}.${year}`;
+
+    if (head === undefined || head.title !== today) {
+      // ggf. neuer Abschnitt für heutiges Datum
+      head = { data: [], title: today };
+      tail = items;
+    }
+    const newItem = { text: text, date: now.getTime() };
+    head.data = [newItem, ...head.data];
+    items = [head, ...tail];
+    this.setState({ items });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <JournalItems items={this.state.items} />
+        <JournalItemInput onSubmit={text => this._addItem(text)} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
